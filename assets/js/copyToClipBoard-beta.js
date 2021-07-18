a41d7d82c7d51a2e7cb0ce352a5425075d9e4e3e
jays1ngh <jaysingh@outlook.com.au>
2021-07-19 09:11:09 +1000

    Improved the code

--- assets/js/copyToClipBoard-beta.js
+++ assets/js/copyToClipBoard-beta.js
@@ -1,24 +1,28 @@
+// Select all pre and code tags
+const codeBlocks = document.querySelectorAll('pre > code');
 
-// Select all pre and code tags and add copy-code-button
-document.querySelectorAll('pre > code').forEach(function (codeBlock) {
+// Add buttons before pre and code tags
+codeBlocks.forEach(function (codeBlock) {
     var button = document.createElement('button');
     button.className = 'copy-code-button';
     button.type = 'button';
-
-    
-
+    // Pre code block
     var pre = codeBlock.parentNode;
-    if (pre.parentNode.classList.contains('highlight')) {
-        var highlight = pre.parentNode;
-        highlight.parentNode.insertBefore(button, highlight);
+    // Highlight Div
+    var highlightDiv = pre.parentNode;
+    
+    if (highlightDiv.classList.contains('highlight')) {
+        // add button before highlight Div if present
+        highlightDiv.parentNode.insertBefore(button, highlightDiv);
     } else {
+        // add copy code button before pre tags
         pre.parentNode.insertBefore(button, pre);
     }
 });
 
-const codeBlocks = document.querySelectorAll('.highlighter-rouge');
+// 
 const copyCodeButtons = document.querySelectorAll('.copy-code-button');
-console.log(copyCodeButtons.innerText);
+
 copyCodeButtons.forEach((copyCodeButton, index) => {
   const code = codeBlocks[index].innerText;
   copyCodeButton.addEventListener('click', () => {
