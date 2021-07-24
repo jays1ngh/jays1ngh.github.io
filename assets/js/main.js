b413e3259b48bcf936daef252c6905a4470bed5d
jays1ngh <jaysingh@outlook.com.au>
2021-07-24 18:18:18 +1000

    updated navbar js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,23 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li, #theme-mode-icon')
+    // Toggle Nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+        // Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 500ms ease forwards ${index / 3 + .05}s`;
+            }
+        });
+
+        // Burger animation
+        burger.classList.toggle(`toggle-burger`);
+    });
+}
+
+navSlide();
